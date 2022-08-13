f0d84001eacc84c3a7906c2b70d6767618331873
Tamilselvan Pattabiraman <111197479+tamilselvan26021989@users.noreply.github.com>
2022-08-14 00:17:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.header('BreakFast Menu') 
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale,spinach & Rocket Smoothie') 
+streamlit.text('Hard-Boiled Free-Range Egg')
